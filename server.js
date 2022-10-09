392eb33fb1b9bf4cbf8f96bd5c0626bb4484ce09
uihalagedara <it20224820@my.sliit.lk>
2022-10-09 19:39:14 +0530

    database connected with middlewares

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
-//server.js
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+//connecting to mongodb atlas database
+const db = require("./models/");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch((err) => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
