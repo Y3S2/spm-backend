6e539482f67f5d69d0fa79e3eed7c0b3f507c869
uihalagedara <it20224820@my.sliit.lk>
2022-10-09 20:02:13 +0530

    Updated server file with port

--- server.js
+++ server.js
@@ -37,3 +37,9 @@ db.mongoose
   app.get("/", (req, res) => {
     res.json({message: "Welcome to ispirithalei."});
   });
+
+  // set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
