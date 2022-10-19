0308ce3551a9e265ae1efc84afdfeddc5787ac48
di-nethra <anjana.dinethra@gmail.com>
2022-10-19 21:17:11 +0530

    post and get routes added

--- routes/employeeDetails.routes.js
+++ routes/employeeDetails.routes.js
@@ -4,6 +4,11 @@ module.exports = (app) => {
   let router = require("express").Router();
 
   // Create a new empform
+  router.post("/", empform.create);
+
+  // Retrieve all empform data with given Emp ID
+  router.get("emp/:id", empform.findAllByEmpID);
+
 
   app.use("/api/empform", router); //this is the base url
 };
