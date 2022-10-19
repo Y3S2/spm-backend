a7a432a48e364c74797b87de3f51ff781997a068
di-nethra <anjana.dinethra@gmail.com>
2022-10-19 21:17:41 +0530

    put and delete operations added

--- routes/employeeDetails.routes.js
+++ routes/employeeDetails.routes.js
@@ -9,6 +9,20 @@ module.exports = (app) => {
   // Retrieve all empform data with given Emp ID
   router.get("emp/:id", empform.findAllByEmpID);
 
+  //find all data
+  router.get("/", empform.getAll);
+
+  //find all doctors
+  router.get("/doctors", empform.getAllDoctors)
+
+  // Update a empform with id
+  router.put("/:id", empform.update);
+
+  // Delete a empform with id
+  router.delete("/:id", empform.delete);
+
+  //find single employee by id
+  router.get("/:id", empform.findOne);
 
   app.use("/api/empform", router); //this is the base url
 };
