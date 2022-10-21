b1486870e3f3eb3fad3731aca5ab98371972e7e8
danushka9999 <dranasinghe99@gmail.com>
2022-10-21 20:58:25 +0530

    create new test controller and routes finished

--- controllers/tests.controller.js
+++ controllers/tests.controller.js
@@ -2,3 +2,14 @@ const db = require("../models");
 const Tests = db.tests;
 
 
+//Retrieve all subbmitted tesrts from the database
+exports.findAllSubbmitted = (req, res) => {
+    Tests.find({ status: "subbmitted" })
+        .then((data) => {
+            res.json(data);
+            //console.log(data);
+        })
+        .catch((err) => {
+            alert(err);
+        });
+};
